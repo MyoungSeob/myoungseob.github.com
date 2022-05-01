a7d07e007532da8e3f6957687d2d0639223ee213
MyoungSeob <woodyms@nate.com>
2022-05-01 23:49:17 +0900

    5/1 알고리즘

--- algorithm_30.js
+++ algorithm_30.js
@@ -6,6 +6,7 @@ function solution(n) {
   threeNum = n.toString(3).split("");
   // 3진법을 반전하지 않고, 자리수 별로 제곱 후 자리수의 숫자만큼 곱해준다면 반전 후 10진법으로 표현한 것과 같아진다.
   for (let i = 0; i < threeNum.length; i++) {
+    // ** 은 거듭제곱을 의미
     answer += 3 ** i * Number(threeNum[i]);
   }
   return answer;
